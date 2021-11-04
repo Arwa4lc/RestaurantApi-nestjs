617b5e6b8859a62084ec0d7ef108471719fbc4b2
Arwa Abdelrahem <arwaabdelrahem22@gmail.com>
2021-11-04 13:46:35 +0200

    creating dummy admin on app startup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
+import { InjectModel, MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
+import { Model } from 'mongoose';
 import { AuthController } from './auth.controller';
-import { userSchema } from './auth.model';
+import { User, userSchema } from './auth.model';
 import { AuthService } from './auth.service';
 import authConfig from './config/auth.config';
 import { JwtStrategy } from './jwt-strategy';
+import * as bcrypt from 'bcrypt';
 
 @Module({
   imports: [
@@ -28,4 +30,26 @@ import { JwtStrategy } from './jwt-strategy';
   providers: [AuthService, JwtStrategy],
   exports: [JwtStrategy, PassportModule],
 })
-export class AuthModule {}
+export class AuthModule {
+  constructor(
+    @InjectModel('User') private readonly User: Model<User>,
+    private configService: ConfigService,
+  ) {
+    this.createAdmin();
+  }
+
+  async createAdmin() {
+    let admin = await this.User.findOne({ role: 'admin' });
+    if (admin) {
+      return console.log('Admin found');
+    }
+
+    admin = await this.User.create({
+      name: 'Arwa abdelrahem',
+      email: this.configService.get('USER_EMAIL'),
+      password: await bcrypt.hash(this.configService.get('USER_PASSWORD'), 12),
+      role: 'admin',
+    });
+    return console.log('Admin created');
+  }
+}
